5247ac54777882deb434a22e2f374f3866e45831
Cr-Israel <carlosisrael08@hotmail.com>
2023-08-31 16:47:00 -0300

    feat: in memory test database to challenges

---
+++ tests/repositories/in-memory-challenges-repository.ts
@@ -0,0 +1,17 @@
+import { ChallengesRepository } from '../../src/aplication/repositories/ChallengesRepository'
+import { Challenge } from '../../src/domain/entities/challenge';
+
+
+export class InMemoryChallengesRepository implements ChallengesRepository {
+    public items: Challenge[] = [];
+    
+    async findById(id: string): Promise<Challenge | null> {
+        const Challenge =  this.items.find(Challenge => Challenge.id === id)
+
+        if(!Challenge) {
+            return null
+        }
+
+        return Challenge
+    }
+}
